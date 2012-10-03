6d92db286a1f53a4e1c4cafc4fd39feb725b55c4
Alexandre Mondaini Calvão <apotema@gmail.com>
2012-10-03 19:40:08 -0300

    Tests refactor

--- spec/requests/entries_spec.rb
+++ spec/requests/entries_spec.rb
@@ -52,11 +52,4 @@ describe "Todo List administration" do
 
   end
 
-  def login user
-    visit login_path
-    fill_in "Login", with: user.login
-    fill_in "Password", with: "123456"
-    click_button "Login"
-  end
-
 end
--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -49,11 +49,4 @@ describe "User administration" do
 
   end
 
-  def login user
-    visit login_path
-    fill_in "Login", with: user.login
-    fill_in "Password", with: "123456"
-    click_button "Login"
-  end
-
 end
